f37850ee254ab5f1a33453ce69e5cd069bbc4237
ArtyomYudin <yudin.artyom@gmail.com>
2025-03-24 17:26:46 +0300

    fix up

--- mariadb/mariadb-install.sh
+++ mariadb/mariadb-install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-kubectl apply -f namespace/mariadb-namespace.yaml
-kubectl apply -f security/rbac.yaml
-kubectl apply -f security/mariadb-secret.yaml
-kubectl apply -f persistentvolumeclaim/mariadb-pvc.yaml
-kubectl apply -f statefulset/mariadb-statefulset.yaml
-kubectl apply -f services/mariadb-service.yaml
+export NAMESPACE=$1
+export ROOT_PASSWORD=$2
+
+envsubst < namespace/mariadb-namespace.yaml | kubectl apply -f -
+envsubst < security/rbac.yaml | kubectl apply -f -
+envsubst < security/mariadb-secret.yaml | kubectl apply -f -
+envsubst < persistentvolumeclaim/mariadb-pvc.yaml | kubectl apply -f -
+envsubst < statefulset/mariadb-statefulset.yaml | kubectl apply -f -
+envsubst < services/mariadb-service.yaml | kubectl apply -f -
--- mariadb/mariadb-uninstall.sh
+++ mariadb/mariadb-uninstall.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-kubectl delete -n mariadb -f security/rbac.yaml
-kubectl delete -f security/mariadb-secret.yaml
-kubectl delete -f statefulset/mariadb-statefulset.yaml
-kubectl delete -f services/mariadb-service.yaml
+export NAMESPACE=$1
+export ROOT_PASSWORD=$2
+
+envsubst < security/rbac.yaml | kubectl delete -f -
+envsubst < security/mariadb-secret.yaml | kubectl delete -f -
+envsubst < statefulset/mariadb-statefulset.yaml | kubectl delete -f -
+envsubst < services/mariadb-service.yaml | kubectl delete -f -
 
 if [[ $# -gt 0  && "$1" == "keeppvc" ]]
 then
     echo "Keeping namespace and persistent volume claim"
 else
-    kubectl delete -f persistentvolumeclaim/mariadb-pvc.yaml
-    kubectl delete namespace mariadb
+    envsubst < persistentvolumeclaim/mariadb-pvc.yaml | kubectl delete -f -
+    kubectl delete namespace $NAMESPACE
 fi
