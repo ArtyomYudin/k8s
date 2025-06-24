cd12789c2fb61c153aad6da80cd908fab1f3eebd
ArtyomYudin <yudin.artyom@gmail.com>
2025-06-24 17:47:26 +0300

    daily work

--- branch-websites/branch-install.sh
+++ branch-websites/branch-install.sh
@@ -12,6 +12,7 @@ envsubst < security/rbac.yaml | kubectl apply -f -
 #envsubst < security/legacy-site-secret.yaml | kubectl apply -f -
 envsubst < persistentvolumeclaim/branch-pvc.yaml | kubectl apply -f -
 envsubst < deployments/php-fpm-deployment.yaml | kubectl apply -f -
+kubectl -n $NAMESPACE create configmap centrinform-wildcard-cert --from-file=server.key=/Volumes/Macintosh\ HD\ Dev/wild\ 2025/key2025.pem --from-file=server.crt=/Volumes/Macintosh\ HD\ Dev/wild\ 2025/chain2025.pem
 #envsubst < deployments/nginx-configmap.yaml | kubectl apply -f -
 envsubst < deployments/nginx-deployment.yaml | kubectl apply -f -
 envsubst < services/php-fpm-service.yaml | kubectl apply -f -
